38d6938379b28925716faf92618b579be4a478bd
Faisal Bhat <plesim18@gmail.com>
2024-01-11 10:22:49 +0000

    Adding Redux

---
+++ src/actions.js
@@ -0,0 +1,5 @@
+// Added At Learning Redux
+export const setSearchField = (text) => ({
+    type:'CHANGE_SEARCH_FIELD', // action to be taken
+    payload:text // whatever data is need to go on reducer 
+})
