64dcb9881e25416b14fe47d7f635dfeafbab0fd8
Faisal Bhat <plesim18@gmail.com>
2024-01-15 09:15:55 +0000

    Added Redux-Logger

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 // Added At Learning Redux
 import { CHANGE_SEARCH_FIELD } from "./constants"
 export const setSearchField = (text) => {
-    console.log(text)
+    // console.log(text)
     return{
         type:CHANGE_SEARCH_FIELD, // action to be taken
         payload:text // whatever data is need to go on reducer 
